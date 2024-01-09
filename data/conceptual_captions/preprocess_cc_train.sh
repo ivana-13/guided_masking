3ce401ad2697b04a43974face3240b57923b5a1a
ivana-13 <67903484+ivana-13@users.noreply.github.com>
2024-01-09 10:08:12 +0100

    Delete data/conceptual_captions directory

--- data/conceptual_captions/preprocess_cc_train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CORPUS="/data/bugliarello.e/conceptual_captions"
-
-source activate volta
-
-python preprocess_cc_train.py $CORPUS
-
-conda deactivate
--- data/conceptual_captions/preprocess_cc_valid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CORPUS="/data/bugliarello.e/conceptual_captions"
-
-source activate volta
-
-python preprocess_cc_valid.py $CORPUS
-
-conda deactivate
